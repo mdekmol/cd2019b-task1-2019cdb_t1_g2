42e023230c1a5ecd119be2fb122b138fbe9a46f3
40623202 <40623202@gm.nfu.edu.tw>
2019-04-12 15:48:15 +0800

    blog test

--- publishconf.py
+++ publishconf.py
@@ -30,7 +30,7 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-DISQUS_SITENAME = "mdecadp2018"
+DISQUS_SITENAME = "mdekmol"
 DISQUS_DISPLAY_COUNTS = False
 #GOOGLE_ANALYTICS = ""
 
